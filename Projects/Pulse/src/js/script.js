4c6b8162e436195dcfd693dd18356e80f2ea7239
Nikolay <savenko.nikolay.valerevich@gmail.com>
2023-04-28 13:04:09 +0300

    add a little lesson on JS

---
+++ Projects/Pulse/src/js/script.js
@@ -0,0 +1,25 @@
+var name = 'Nikolay';
+
+let number = 31;
+const pi = 3.14;
+
+number = 4;
+
+let leftBorderWidth = 200;
+
+// number
+//string
+/* true/false */
+//null
+/* let obj {
+    name: 'apple',
+    color: 'green',
+    weight: 200
+} */
+
+/* alert(1234); */
+
+/* let answer = confirm("Вам есть 18 лет?");
+console.log(answer); */
+let answer = prompt("Вам есть 18 лет?", '');
+console.log(answer);
