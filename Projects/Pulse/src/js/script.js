6570803bc57bb903d2099c0a191b4d43308cb637
Nikolay <savenko.nikolay.valerevich@gmail.com>
2023-05-02 11:52:33 +0300

    add jquery slick and start new section carousel

--- Projects/Pulse/src/js/script.js
+++ Projects/Pulse/src/js/script.js
@@ -1,25 +1,9 @@
-var name = 'Nikolay';
-
-let number = 31;
-const pi = 3.14;
-
-number = 4;
-
-let leftBorderWidth = 200;
-
-// number
-//string
-/* true/false */
-//null
-/* let obj {
-    name: 'apple',
-    color: 'green',
-    weight: 200
-} */
-
-/* alert(1234); */
-
-/* let answer = confirm("Вам есть 18 лет?");
-console.log(answer); */
-let answer = prompt("Вам есть 18 лет?", '');
-console.log(answer);
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+      });
+});
