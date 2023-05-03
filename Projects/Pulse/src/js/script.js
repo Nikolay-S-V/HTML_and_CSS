4d5c6c36ae35019fa772c9538bfb6bed0b65bdbf
Nikolay <savenko.nikolay.valerevich@gmail.com>
2023-05-03 13:34:42 +0300

    add tiny-slider

--- Projects/Pulse/src/js/script.js
+++ Projects/Pulse/src/js/script.js
@@ -1,9 +1,8 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        /* adaptiveHeight: true, */
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/carousel/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/carousel/right.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/carousel/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/carousel/right.svg"></button>',
         responsive: [
             {
                 breakpoint: 768,
@@ -14,4 +13,21 @@ $(document).ready(function(){
             }
         ]
       });
+}); */
+
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls: false,
+  nav: false
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
 });
