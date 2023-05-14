7c8ccccd2006bdd090e133da2b864ad592384e92
Nikolay <savenko.nikolay.valerevich@gmail.com>
2023-05-14 12:09:33 +0300

    added modal script and finished 2/3 section 'overlay'

--- Projects/Pulse/src/js/script.js
+++ Projects/Pulse/src/js/script.js
@@ -38,3 +38,17 @@ $('.catalog-item__back').each(function(i) {
   })
 });
 
+//modal
+$('[data-modal=consultation]').on('click', function() {
+  $('.overlay, #consultation').fadeIn('slow');
+});
+$('.modal__close').on('click', function() {
+  $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+});
+$('.button_mini').each(function(i) {
+  $(this).on('click', function() {
+    $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+    $('.overlay, #order').fadeIn('slow');
+  })
+});
+
