02514978888c086eb7f16ac66b1629b525eca66a
Nikolay <savenko.nikolay.valerevich@gmail.com>
2023-05-19 16:53:42 +0300

    add 4x4

---
+++ Work/boy or girl/JS/script.js
@@ -0,0 +1,36 @@
+var card = document.querySelector('.card');
+card.addEventListener( 'click', function() {
+  card.classList.toggle('is-flipped');
+});
+var card_2 = document.querySelector('.card_2');
+card_2.addEventListener( 'click', function() {
+  card_2.classList.toggle('is-flipped');
+});
+var card_3 = document.querySelector('.card_3');
+card_3.addEventListener( 'click', function() {
+  card_3.classList.toggle('is-flipped');
+});
+var card_4 = document.querySelector('.card_4');
+card_4.addEventListener( 'click', function() {
+  card_4.classList.toggle('is-flipped');
+});
+var card_5 = document.querySelector('.card_5');
+card_5.addEventListener( 'click', function() {
+  card_5.classList.toggle('is-flipped');
+});
+var card_6 = document.querySelector('.card_6');
+card_6.addEventListener( 'click', function() {
+  card_6.classList.toggle('is-flipped');
+});
+var card_7 = document.querySelector('.card_7');
+card_7.addEventListener( 'click', function() {
+  card_7.classList.toggle('is-flipped');
+});
+var card_8 = document.querySelector('.card_8');
+card_8.addEventListener( 'click', function() {
+  card_8.classList.toggle('is-flipped');
+});
+var card_9 = document.querySelector('.card_9');
+card_9.addEventListener( 'click', function() {
+  card_9.classList.toggle('is-flipped');
+});
